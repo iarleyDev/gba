af664abeade4e6619d00c16eb66f756959c08e28
Iarley <120318783+iarleyDev@users.noreply.github.com>
2023-02-04 13:43:08 -0300

    CPU::C: Defining PTRS (Pointers) and Console

---
+++ franchises/pokemon/firered/Console.java
@@ -0,0 +1,9 @@
+// C_GBA = 3,
+// Console GBA = 3 Indexing
+public class Console {
+  
+  public static int final C_GBA_3 = 0X000001;
+  public static final int PTR_DEFAULT = 0X000000012;
+  public static final int PTR_LAST = 0x00000000032;
+  public static final int PTR_END = 0x000000050;
+}
